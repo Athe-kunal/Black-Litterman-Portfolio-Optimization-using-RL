6df9483010901c52af1e8cbe6cc0ce565a9726dd
Astarag Mohapatra <40949756+Athe-kunal@users.noreply.github.com>
2023-06-13 02:53:05 -0400

    Update config_params.py

--- config_params.py
+++ config_params.py
@@ -1,8 +1,8 @@
 num_workers = 3
 num_samples = 100
 num_gpus = 1
-training_iterations = 1000
-checkpoint_freq = 250
+training_iterations = 500
+checkpoint_freq = 100
 
 WANDB_PROJECT = "FINRL_RUN"
 WANDB_API_KEY = "70e95a405ea4aec8d0a637460407bf21c69436f4"
